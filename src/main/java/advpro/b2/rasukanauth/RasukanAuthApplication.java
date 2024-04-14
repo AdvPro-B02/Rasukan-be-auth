8f777a5d50c61459360d71a58f7475f785f5c319
MightyZanark <40352213+MightyZanark@users.noreply.github.com>
2024-04-15 00:15:47 +0900

    initial commit

---
+++ src/main/java/advpro/b2/rasukanauth/RasukanAuthApplication.java
@@ -0,0 +1,13 @@
+package advpro.b2.rasukanauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RasukanAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RasukanAuthApplication.class, args);
+    }
+
+}
