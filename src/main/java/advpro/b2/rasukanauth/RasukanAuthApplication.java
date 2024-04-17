0f0b6673fa6b8c4d0bba8817dcbef2f94865f479
MightyZanark <40352213+MightyZanark@users.noreply.github.com>
2024-04-17 20:11:35 +0900

    fix: exclude DataSourceAutoConfiguration class from application

--- src/main/java/advpro/b2/rasukanauth/RasukanAuthApplication.java
+++ src/main/java/advpro/b2/rasukanauth/RasukanAuthApplication.java
@@ -2,8 +2,9 @@ package advpro.b2.rasukanauth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class RasukanAuthApplication {
 
     public static void main(String[] args) {
