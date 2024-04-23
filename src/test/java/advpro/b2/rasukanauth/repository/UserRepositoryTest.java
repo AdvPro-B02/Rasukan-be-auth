454e4bfb4fbd4f6ca4c64313e7448ee2d3fc7d18
MightyZanark <40352213+MightyZanark@users.noreply.github.com>
2024-04-23 23:56:56 +0900

    [RED] Create tests for UserRepository

---
+++ src/test/java/advpro/b2/rasukanauth/repository/UserRepositoryTest.java
@@ -0,0 +1,14 @@
+package advpro.b2.rasukanauth.repository;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+public class UserRepositoryTest {
+
+    @BeforeEach
+    void setUp() {}
+}
