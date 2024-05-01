7f1dc520afe23a025720e3947ee38738829053c6
MightyZanark <40352213+MightyZanark@users.noreply.github.com>
2024-05-01 21:47:11 +0900

    [GREEN] Implement login and modified register on AuthController to pass tests

--- src/main/java/advpro/b2/rasukanauth/controller/AuthController.java
+++ src/main/java/advpro/b2/rasukanauth/controller/AuthController.java
@@ -2,12 +2,15 @@ package advpro.b2.rasukanauth.controller;
 
 import advpro.b2.rasukanauth.model.builder.UserBuilder;
 import advpro.b2.rasukanauth.service.AuthService;
-import advpro.b2.rasukanauth.service.AuthServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
+import java.util.NoSuchElementException;
+
+@RestController
 @RequestMapping("/auth")
 class AuthController {
 
@@ -15,15 +18,26 @@ class AuthController {
     AuthService authService;
 
     @PostMapping("/register")
-    @ResponseBody
-    public String register(@ModelAttribute UserBuilder userBuilder) {
-        return authService.register(userBuilder);
+    public ResponseEntity<String> register(@ModelAttribute UserBuilder userBuilder) {
+        try {
+            authService.register(userBuilder);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     @PostMapping("/login")
-    @ResponseBody
-    public String login() {
-        return "Hi, this API currently is not functional. Thanks for the interest";
+    public ResponseEntity<String> login(@RequestParam String email, @RequestParam String password) {
+        String token;
+        try {
+            token = authService.login(email, password);
+        } catch (NoSuchElementException e) {
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Bearer " + token);
+        return new ResponseEntity<>(headers, HttpStatus.OK);
     }
 
     @PostMapping("/logout")
