f89202d5f374238a6d16aa7de7c212c639734152
MightyZanark <40352213+MightyZanark@users.noreply.github.com>
2024-05-23 23:26:57 +0900

    [RED] Add tests for get/update user balance, get user by id, get all user on UserService

--- src/test/java/advpro/b2/rasukanauth/service/UserServiceTest.java
+++ src/test/java/advpro/b2/rasukanauth/service/UserServiceTest.java
@@ -12,8 +12,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import java.util.NoSuchElementException;
-import java.util.UUID;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -64,13 +63,42 @@ public class UserServiceTest {
     }
 
     @Test
-    void testGetUserById() {
-        assertNull(userService.getUserById());
+    void testGetUserById_success() {
+        Optional<User> userOpt = Optional.of(testUser);
+        doReturn(userOpt).when(userRepository).findById(any(UUID.class));
+
+        User user = userService.getUserById(testUser.getId().toString());
+        assertEquals(testUser.getId(), user.getId());
+        assertEquals(testUser.getName(), user.getName());
+        assertEquals(testUser.getEmail(), user.getEmail());
+        assertEquals(testUser.getPassword(), user.getPassword());
+        assertEquals(testUser.isStaff(), user.isStaff());
+    }
+
+    @Test
+    void testGetUserById_fail() {
+        doReturn(Optional.empty()).when(userRepository).findById(any(UUID.class));
+
+        User user = userService.getUserById(testUser.getId().toString());
+        assertNull(user);
     }
 
     @Test
     void testGetAllUsers() {
-        assertNull(userService.getAllUser());
+        List<User> userList = new ArrayList<>(5);
+        for (int i = 0; i < 5; i++) {
+            userBuilder.setId(UUID.randomUUID());
+            userBuilder.setEmail(String.format("service%d@test.com", i));
+            userList.add(userBuilder.build());
+        }
+
+        doReturn(userList).when(userRepository).findAll();
+        List<User> allUser = userService.getAllUser();
+        assertEquals(userList.size(), allUser.size());
+        for (int i = 0; i < userList.size(); i++) {
+            assertEquals(userList.get(i).getId(), allUser.get(i).getId());
+            assertEquals(userList.get(i).getEmail(), allUser.get(i).getEmail());
+        }
     }
 
     @Test
@@ -91,5 +119,49 @@ public class UserServiceTest {
                 () -> userService.getUserByEmailAndPassword("service@service.com", "servicetest")
         );
         verify(userRepository, times(1)).findByEmailAndPassword(any(String.class), any(String.class));
+    }
+
+    @Test
+    void testGetUserBalance_userExists() {
+        int balance = 100;
+        userBuilder.setBalance(balance);
+        User user = userBuilder.build();
+        Optional<User> userOpt = Optional.of(user);
+
+        doReturn(userOpt).when(userRepository).findById(any(UUID.class));
+        int userBalance = userService.getUserBalance(user.getId().toString());
+        assertEquals(balance, userBalance);
+    }
+
+    @Test
+    void testGetUserBalance_userDoesNotExists() {
+        doReturn(Optional.empty()).when(userRepository).findById(any(UUID.class));
+
+        assertThrows(NoSuchElementException.class, () -> {
+            userService.getUserBalance("68e1011c-0021-4ae9-9d11-0deabf9cb449");
+        });
+    }
+
+    @Test
+    void testUpdateUserBalance_userExists() {
+        int newBalance = 100;
+        doReturn(Optional.of(testUser)).when(userRepository).findById(any(UUID.class));
+        userBuilder.setBalance(newBalance);
+        User user = userBuilder.build();
+        doReturn(user).when(userRepository).save(any(User.class));
+
+        assertNotEquals(newBalance, testUser.getBalance());
+        User updatedUser = userService.updateUserBalance(testUser.getId().toString(), newBalance);
+        assertEquals(testUser.getId(), updatedUser.getId());
+        assertEquals(newBalance, updatedUser.getBalance());
+    }
+
+    @Test
+    void testUpdateUserBalance_userDoesNotExists() {
+        doReturn(Optional.empty()).when(userRepository).findById(any(UUID.class));
+
+        assertThrows(NoSuchElementException.class, () -> {
+            userService.updateUserBalance("68e1011c-0021-4ae9-9d11-0deabf9cb449", 100);
+        });
     }
 }
