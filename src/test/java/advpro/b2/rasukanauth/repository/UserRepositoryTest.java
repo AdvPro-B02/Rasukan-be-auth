de1ea50375b8112249b0df3a476769b628de6a07
MightyZanark <40352213+MightyZanark@users.noreply.github.com>
2024-05-26 18:31:16 +0900

    [REFACTOR] Remove unnecessary imports and reduce code duplications

--- src/test/java/advpro/b2/rasukanauth/repository/UserRepositoryTest.java
+++ src/test/java/advpro/b2/rasukanauth/repository/UserRepositoryTest.java
@@ -5,17 +5,13 @@ import advpro.b2.rasukanauth.model.builder.UserBuilder;
 import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
 
 @DataJpaTest
 @Transactional
